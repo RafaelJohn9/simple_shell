1779d2c8c1266af1591df4d1e4a84e471cd7a19c
RafaelJohn9 <rafaeljohb@gmail.com>
2023-05-26 01:58:33 +0300

    fixed bugs

---
+++ executing.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * executing - this is theexecuting phase
+ * @path: the path of execution
+ * @argv: the whole command line
+ * @envp: the environment
+ */
+void executing(char *path, char **argv, char **envp)
+{
+	int err;
+
+	if (fork() == 0)
+	{
+		err = execve(path,  argv, envp);
+		if (err == -1)
+			_err(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	else
+		wait(NULL);
+}
